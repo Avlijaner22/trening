da90fdc82914e937825fde38621effc14a8846a3
marko <jaksic22marko@yahoo.com>
2015-10-15 01:10:23 +0200

    kraj

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml', '~> 4.0.5'
 gem 'simple_form', '~> 3.0.2'
+gem 'social-share-button', '~> 0.1.6'
 
 
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'homes/show'
+
   resources :workouts do 
     resources :exercises
   end
   root 'workouts#index'
+Rails.application.routes.draw do
+  resources :homes, only: [:show]
+  root to: "homes#show"
+end
 end
